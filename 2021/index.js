b4d412377523e73dc6a4fa1557d99fcf2c67f53c
Vincent Schmandt <vincentscode@gmail.com>
2021-02-25 18:28:53 +0100

    boilerplate

--- 2021/index.js
+++ 2021/index.js
@@ -1,5 +1,24 @@
-function test() {
+const fs = require('fs')
 
-}
+// settings
+const inDir = 'data/'
+const outDir = 'out/'
 
-test("asd")
+const inFile = 'a.txt'
+const outFile = 'a.txt'
+
+// load data
+// data_files = fs.readdirSync(inDir)
+// console.log("Data Files:", data_files)
+console.log("Processing:", inFile)
+const data = fs.readFileSync(inDir + inFile, 'utf8')
+console.log(data)
+
+
+// process data
+
+
+
+// write data
+fs.writeFileSync(outDir + outFile, 'test')
+console.log("Written:", outFile)
